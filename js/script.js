c83fe4e996ccdb64e6f1382697109af577f3b685
Ety Tania <155320189+EtyTania@users.noreply.github.com>
2024-04-03 02:42:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//jq
+$(document).ready(function(){
+
+//	$('button').click(function(){
+	//alert("Hope ypu are doing well.");	
+	//})
+	$('#btn').on('click', function(){
+	alert("Hope ypu are doing well.");	
+	})
+
+	
+})
+
+$(document).ready(function(){
+//$(selector).countMe(delay,speed)
+
+$("#counter").countMe(20,75);
+})
+$(document).ready(function(){
+//$(selector).countMe(delay,speed)
+
+$("#counter1").countMe(20,65);
+})
+$(document).ready(function(){
+//$(selector).countMe(delay,speed)
+
+$("#counter2").countMe(35,75);
+})
